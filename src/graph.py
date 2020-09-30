1f6536d6f70199e569b406a32fcbcfdfcea44aad
Lucas <lucasfcm9@gmail.com>
2020-09-30 15:06:56 -0300

    add floyd warshall's algorithm and menu

--- src/graph.py
+++ src/graph.py
@@ -2,6 +2,8 @@ import json, math
 import gol_flights, graph_algorithms
 import networkx as nx
 import matplotlib.pyplot as plt
+import pprint
+import os
 
 def create_graph():
   with open('../gol_flights_json') as json_file:
@@ -15,6 +17,14 @@ def create_graph():
     to = {'latitude': dt['LatDest'][key], 'longitude': dt['LongDest'][key]}
     graph[value][dt['Aeroporto.Destino'][key]] = gol_flights.distance(_from, to)
   return graph
+
+def list_airports(graph):
+  airports = set()
+  for _from in graph:
+    airports.add(_from)
+    for to in graph[_from]:
+      airports.add(to)
+  return airports
 
 def plot_graph(path):
   G = nx.DiGraph()
@@ -65,15 +75,49 @@ def plot_graph_shortest_path(path, cost):
 
 def main():
   graph = create_graph()
-  path = graph_algorithms.bfs(graph, 'Santos Dumont', 'Leite Lopes')
-  path = graph_algorithms.sp(path, 'Santos Dumont', 'Leite Lopes')
-# minimum spanning tree  
-  mst = graph_algorithms.mst(graph, 'Santos Dumont')
-  print(mst)
-# djikstra
-  graph_algorithms.djikstra(graph, 'Santos Dumont')
-  djikstra_path = graph_algorithms.shortest_path_using_djikstra(graph, 'Santos Dumont', 'Leite Lopes')
-  plot_graph_shortest_path(djikstra_path[0], djikstra_path[1])
+
+  loop_condition = True
+  os.system('cls' if os.name == 'nt' else 'clear')
+  while loop_condition:
+    print('1 - List all airports')
+    print('2 - Use djikstra\'s algorithm to find the shortest path between two airports')
+    print('3 - Generate the minimum spanning tree using prim\'s algorithm')
+    print('4 - Use floyd warshall\'s algorithm to find the minimum distance between two airports')
+    print('5 - Execution time floyd warshall\'s algorithm for all airports')
+    option = input('Enter the option: ')
+    if option == '1':
+      airports = list_airports(graph)
+      pp = pprint.PrettyPrinter(indent=8)
+      pp.pprint(airports)
+    elif option == '2':
+      try:
+        input1 = input('Enter the first airport: ')
+        input2 = input('Enter the second airport: ')
+        graph_algorithms.djikstra(graph, input1)
+        djikstra_path = graph_algorithms.shortest_path_using_djikstra(graph, input1, input2)
+        print('The shortest path and the minimum distance is:')
+        print(djikstra_path[0], djikstra_path[1])
+        plot_graph_shortest_path(djikstra_path[0], djikstra_path[1])
+      except KeyError:
+        print('This place does not exist, enter a valid place, please')
+    elif option == '3':
+      input1 = input('Enter the airport to generate the MST (Minimum Spanning Tree) from this airport: ')
+      mst = graph_algorithms.mst(graph, input1)
+      print('This is a mst from airport %s' % input1)
+      print(dict(mst))
+    elif option == '4':
+      dist, pred = graph_algorithms.floyd_warshall(graph)
+      input1 = input('Enter the first airport: ')
+      input2 = input('Enter the second airport: ')
+      try:
+        print('The minimum distance between ' + input1 + ' and ' + input2 + ' is: %f' % dist[input1][input2] + ' km')
+      except KeyError:
+        print('This place does not exist, enter a valid place, please')
+    elif option == '5':
+      print('The execution time of floyd warshall\'s algorithm for all graph is: ' + str(graph_algorithms.get_execution_time_floyd_warshall(graph)) + ' seconds')
+    elif option == '0':
+      loop_condition = False
+
 
 if __name__ == '__main__':
   main()
--- src/graph_algorithms.py
+++ src/graph_algorithms.py
@@ -1,41 +1,5 @@
 from collections import defaultdict
-import heapq, queue
-
-def dfs(graph, visited, node):
-  visited[node] = True
-  for u in graph[node]:
-    if u not in visited.keys():
-      dfs(graph, visited, u)
-
-def bfs(graph, _from, to):
-  visited = {}
-  queue = []
-  parent = {}
-  queue.append(_from)
-  visited[_from] = True
-
-  while queue:
-    node = queue.pop(0)
-    sp = []
-
-    for u in graph[node]:
-      if u not in visited.keys():
-        if node not in queue:
-          visited[u] = True
-          queue.append(u)
-          parent[u] = node
-          if (u == to):
-            return parent
-
-def sp(parent, _from, to):
-  sp = [to]
-  while sp[-1] != _from:
-    sp.append(parent[sp[-1]])
-  sp.reverse()
-  sp_map = {}
-  for i, node in enumerate(sp[:-1]):
-    sp_map[node] = sp[i + 1]
-  return sp_map
+import heapq, time
 
 def djikstra(graph, _from, to=None):
   try:
@@ -72,6 +36,35 @@ def shortest_path_using_djikstra(graph, _from, to):
     return path_map, d[to]
   except KeyError:
     print('This place does not exist')
+
+def floyd_warshall(graph):
+  try:
+    dist = {}
+    pred = {}
+    for u in graph:
+      dist[u] = {}
+      pred[u] = {}
+      for v in graph:
+        dist[u][v] = float('inf')
+        pred[u][v] = -1
+        dist[u][u] = 0
+        for z in graph[u]:
+          dist[u][z] = graph[u][z]
+          pred[u][z] = u
+    for k in graph:
+      for i in graph:
+        for j in graph:
+          if dist[i][k] + dist[k][j] < dist[i][j]:
+            dist[i][j] = dist[i][k] + dist[k][j]
+    return dist, pred
+  except KeyError:
+    print('This place does not exist')
+
+def get_execution_time_floyd_warshall(graph):
+  start = time.time()
+  dist, pred = floyd_warshall(graph)
+  end = time.time()
+  return end - start
 
 def mst(graph, start):
   mst = defaultdict(set)
